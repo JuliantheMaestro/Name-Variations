4da3a1bbc0941bb32a192834f74bda0b0ad29dc4
Jewlun <julian.boehnertkrueger@gmail.com>
2025-02-16 21:37:40 -0500

    Added querySelector

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const answer3 = document.querySelector("#answer3");
 const answer4 = document.querySelector("#answer4");
 const answer5 = document.querySelector("#answer5");
 const answer6 = document.querySelector("#answer6");
+
+
+const name = document.querySelector("#your-name");
